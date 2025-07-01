d7effcddf02b0cda68f3f71eeb9f33781a906f01
start.vaadin.com <no-reply@vaadin.com>
2025-07-01 22:36:20 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/indicadores/IndicadoresView.java
@@ -0,0 +1,35 @@
+package com.example.application.views.indicadores;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+import org.vaadin.lineawesome.LineAwesomeIconUrl;
+
+@PageTitle("indicadores")
+@Route("")
+@Menu(order = 0, icon = LineAwesomeIconUrl.PENCIL_RULER_SOLID)
+public class IndicadoresView extends Composite<VerticalLayout> {
+
+    public IndicadoresView() {
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        HorizontalLayout layoutRow2 = new HorizontalLayout();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        layoutRow.setWidthFull();
+        getContent().setFlexGrow(1.0, layoutRow);
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setWidth("100%");
+        layoutRow.getStyle().set("flex-grow", "1");
+        layoutRow2.setWidthFull();
+        getContent().setFlexGrow(1.0, layoutRow2);
+        layoutRow2.addClassName(Gap.MEDIUM);
+        layoutRow2.setWidth("100%");
+        layoutRow2.getStyle().set("flex-grow", "1");
+        getContent().add(layoutRow);
+        getContent().add(layoutRow2);
+    }
+}
